6cb3c226793f70497317ac627ee2bc48538b63b2
SJ2050 <sj2050@vip.163.com>
2020-02-15 23:26:42 +0800

    update NWPU README.md

--- NWPU_version/automatic.sh
+++ NWPU_version/automatic.sh
@@ -6,7 +6,7 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 crontab -l > conf
-echo "0 6 * * * python ./say_goodbye_to_ncov.py" >> conf
+echo "0 6 * * * python /home/say_goodbye_to_ncov.py" >> conf
 crontab conf
 rm -f conf
 echo -e "疫情自动填报程序会在每天的6点运行! \a \n"
